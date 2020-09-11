eb7d619eb7b0bc27ecac94082fb8b9a7ddbaeb79
John Hardy <jh@lagado.com>
2020-09-12 01:02:03 +1000

    looping

---
+++ docs/loop.ts
@@ -0,0 +1,37 @@
+import { Module } from 'binaryen';
+import binaryen from 'binaryen';
+
+const m: Module = new binaryen.Module();
+
+m.addFunction(
+  'add',
+  binaryen.createType([]),
+  binaryen.i32,
+  [binaryen.i32, binaryen.i32],
+  m.block(null as any, [
+    m.local.set(0, m.i32.const(10)),
+    m.local.set(1, m.i32.const(0)),
+    m.block('afterLoop', [
+      m.loop(
+        'loop',
+        m.block(null as any, [
+          m.local.set(0, m.i32.sub(m.local.get(0, binaryen.i32), m.i32.const(1))),
+          m.local.set(1, m.i32.add(m.local.get(1, binaryen.i32), m.i32.const(1))),
+          m.br('afterLoop', m.i32.eqz(m.local.get(0, binaryen.i32))),
+          m.br('loop'),
+        ]),
+      ),
+    ]),
+    m.return(m.local.get(1, binaryen.i32)),
+  ]),
+);
+m.addFunctionExport('add', 'add');
+if (!m.validate()) throw new Error('validation error');
+
+console.log('raw:', m.emitText());
+m.optimize();
+console.log('opt:', m.emitText());
+
+const compiled = new WebAssembly.Module(m.emitBinary());
+const instance: any = new WebAssembly.Instance(compiled, {});
+console.log(instance.exports.add(41, 1));
--- src/libs/loop-lib.ts
+++ src/libs/loop-lib.ts
@@ -1,21 +1,46 @@
-import { i32 } from 'binaryen';
+import { auto, i32, none } from 'binaryen';
 import { LibFunc } from '../types';
 import { ioLib } from './io-lib';
 import { builtin } from '../typedefs';
 
 export const loopLib: LibFunc = ({ lib, func, literal, module: m }) => {
   const { log } = lib(ioLib);
+  const get = builtin(m.local.get, i32);
+  const set = builtin(m.local.set, i32);
+  const drop = builtin(m.drop, none);
   const sub = builtin(m.i32.sub, i32);
+  const block = builtin(m.block, auto);
+  const loop = builtin(m.loop, none);
+  const br = builtin(m.br, none);
 
   const looper = func(
     { params: { a: i32, b: i32 } },
 
-    ({ $, exec }) => {
-      $.i = literal(10);
-      $.i = sub($.i, literal(1));
-      exec(
-        // m.drop(($.i = sub($.i,1))),
+    ({ $, exec, result }) => {
+      $.i = literal(1);
+      result(
+        log(
+          block('while', [
+            set(2, sub(get(2, i32), literal(1))),
+            // ($.i = sub($.i, literal(1))),
+            m.br('while', 1, literal(2000)),
+            (m.i32.eq(literal(0), literal(0))),
+            // literal(m.i32.ne(literal(0), literal(0)), literal(2000)),
+            // literal(5000),
+            // $.i,
+            // literal(1000),
+            // log(($.i = sub($.i, literal(1)))),
+            //   // (loop as any)('loop', [
+            //   //   //
+            //   //   // log($.i),
+            //   br('while', ($.i = sub($.i, literal(1)))),
+            //   log(literal(50)),
+            // br('loop'),
+            //   // ]),
+          ]),
+        ),
         log($.i),
+        log(literal(100)),
       );
     },
   );
