aedbeb891d7903337236b1e06118bfb41cf54fac
John Hardy <john.hardy@workjam.com>
2020-09-17 00:22:30 +1000

    restored life code

--- src/life/asm/life.ts
+++ src/life/asm/life.ts
@@ -2,11 +2,11 @@ import { i32 } from 'binaryen';
 import { builtin, LibFunc } from '../../esential';
 
 export const lifeLib: LibFunc = ({ module, func, literal, FOR }) => {
-  const load = builtin(module.i32.load, i32);
+  // const load = builtin(module.i32.load, i32);
   const store = builtin(module.i32.store, i32);
 
-  const init = func({ params: { width: i32, height: i32 } }, ({ $, result }) => {
-    // store(0, 0, 0, 0xffffffff);
+  const init = func({ params: { width: i32, height: i32 } }, ({ result }) => {
+    store(0, 0, 0, 0xffffffff);
     result(literal(1000));
   });
 
--- src/life/index.ts
+++ src/life/index.ts
@@ -1,9 +1,6 @@
-// import { addListeners, calcNumPages, rgb2bgr } from './utils';
-// import { Exported } from './types';
-// import { RGB_ALIVE, RGB_DEAD, BIT_ROT, WASM_FILENAME, HEIGHT, WIDTH } from './common/constants';
-
-import { HEIGHT, WIDTH } from './common/constants';
-import { calcNumPages } from './utils';
+import { HEIGHT, RGB_ALIVE, RGB_DEAD, BIT_ROT, WASM_FILENAME, WIDTH } from './common/constants';
+import { Exported } from './types';
+import { calcNumPages, rgb2bgr } from './utils';
 
 // const addAllListeners = (canvas: HTMLCanvasElement, document: Document) => {
 //   // When clicked or dragged, fill the current row and column with random live cells
@@ -64,62 +61,58 @@ const run = async (canvas: HTMLCanvasElement) => {
 `;
 
   const boardSize = WIDTH * HEIGHT;
-  // const memory = new WebAssembly.Memory({
-  //   initial: 1,
-  //   // initial: calcNumPages(500, 500),
-  // });
-  console.log(calcNumPages(500, 500));
-  //   try {
-  //     const response = await fetch(WASM_FILENAME);
-  //     const buffer = await response.arrayBuffer();
-  //     // const module = await WebAssembly.instantiate(buffer, {
-  //     //   env: {
-  //     //     memory,
-  //     //     abort: function() {
-  //     //       return;
-  //     //     },
-  //     //   },
-  //     //   config: {
-  //     //     BGR_ALIVE: rgb2bgr(RGB_ALIVE) | 1, // little endian, LSB must be set
-  //     //     BGR_DEAD: rgb2bgr(RGB_DEAD) & ~1, // little endian, LSB must not be set
-  //     //     BIT_ROT,
-  //     //   },
-  //     //   Math: Math as any,
-  //     // });
+  const memory = new WebAssembly.Memory({
+    initial: calcNumPages(500, 500),
+  });
+  try {
+    const response = await fetch(WASM_FILENAME);
+    const buffer = await response.arrayBuffer();
+    const module = await WebAssembly.instantiate(buffer, {
+      env: {
+        memory,
+        abort: function() {
+          return;
+        },
+      },
+      config: {
+        BGR_ALIVE: rgb2bgr(RGB_ALIVE) | 1, // little endian, LSB must be set
+        BGR_DEAD: rgb2bgr(RGB_DEAD) & ~1, // little endian, LSB must not be set
+        BIT_ROT,
+      },
+      Math: Math as any,
+    });
 
-  //     // const exports = module.instance.exports as Exported;
+    const exports = module.instance.exports as Exported;
 
-  //     // const sanity = exports.init(WIDTH, HEIGHT);
-  //     // if (sanity !== 1000) {
-  //     //   throw new Error(`Couldn't initialise wasm file`);
-  //     // }
-  //     // console.log('wasm loaded');
+    const sanity = exports.init(WIDTH, HEIGHT);
+    if (sanity !== 1000) {
+      throw new Error(`Couldn't initialise wasm file`);
+    }
+    console.log('wasm loaded');
 
-  //     // const mem = new Uint32Array(memory.buffer);
+    const mem = new Uint32Array(memory.buffer);
 
-  //     // // Update about 30 times a second
-  //     // (function update() {
-  //     //   setTimeout(update, 1000 / 30);
-  //     //   mem.copyWithin(0, boardSize, boardSize + boardSize); // copy output to input
-  //     //   exports.step(); // perform the next step
-  //     // })();
+    // Update about 30 times a second
+    (function update() {
+      setTimeout(update, 1000 / 30);
+      mem.copyWithin(0, boardSize, boardSize + boardSize); // copy output to input
+      exports.step(); // perform the next step
+    })();
 
-  //     // // Keep rendering the output at [size, 2*size]
-  //     // const imageData = ctx.createImageData(WIDTH, HEIGHT);
-  //     // const argb = new Uint32Array(imageData.data.buffer);
-  //     // (function render() {
-  //     //   requestAnimationFrame(render);
-  //     //   argb.set(mem.subarray(boardSize, boardSize + boardSize)); // copy output to image buffer
-  //     //   ctx.putImageData(imageData, 0, 0); // apply image buffer
-  //     // })();
+    // Keep rendering the output at [size, 2*size]
+    const imageData = ctx.createImageData(WIDTH, HEIGHT);
+    const argb = new Uint32Array(imageData.data.buffer);
+    (function render() {
+      requestAnimationFrame(render);
+      argb.set(mem.subarray(boardSize, boardSize + boardSize)); // copy output to image buffer
+      ctx.putImageData(imageData, 0, 0); // apply image buffer
+    })();
 
-  //     // addAllListeners(canvas, document);
-  //   } catch (err) {
-  //     alert('Failed to load WASM: ' + err.message + ' (ad blocker, maybe?)');
-  //     console.log(err.stack);
-  //   }
+    // addAllListeners(canvas, document);
+  } catch (err) {
+    alert('Failed to load WASM: ' + err.message + ' (ad blocker, maybe?)');
+    console.log(err.stack);
+  }
 };
 
 run(document.getElementsByTagName('canvas')[0]);
-
-console.log('huh?');
