ff594799a4664ca2871ae7565fed988b8b847871
John Hardy <john.hardy@workjam.com>
2020-09-24 00:37:12 +1000

    save

--- docs/hello.ts
+++ docs/hello.ts
@@ -9,8 +9,8 @@ lib(({ func }) => {
   const addition = func({ params: { a: i32, b: i32 } }, ({ vars: { a, b, u }, result }) => {
     result(
       //
-      u(add(a(), b())),
-      u(),
+      u(add(a, b)),
+      u,
     );
   });
   return {
