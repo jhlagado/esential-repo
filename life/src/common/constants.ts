2fc3a181ad3a01806a156f026f90dd7bb5f331a1
John Hardy <john.hardy@workjam.com>
2020-09-29 00:42:56 +1000

    save

--- life/src/common/constants.ts
+++ life/src/common/constants.ts
@@ -2,6 +2,6 @@ export const RGB_ALIVE = 0xffffc0c1;
 export const RGB_DEAD = 0xffa50a00;
 export const BIT_ROT = 10;
 
-export const WASM_FILENAME = 'esential-repo/life.wasm';
+export const WASM_FILENAME = 'life.wasm';
 export const WIDTH = 500;
 export const HEIGHT = 500;
